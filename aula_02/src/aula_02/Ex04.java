cb5842c0692bcafee92956ce386a0049b09277e9
pedro-hsb <pedro.bernardo4561@gmail.com>
2023-11-09 17:39:07 -0300

    aula0911

--- aula_02/src/aula_02/Ex04.java
+++ aula_02/src/aula_02/Ex04.java
@@ -7,13 +7,16 @@ public class Ex04 {
 	public static void main(String[] args) {
 		float n1,n2,n3,n4,resultado;
 		// TODO Auto-generated method stub
-		System.out.println("");
+		System.out.println("digite o primeiro numero:");
 		n1=leia.nextFloat();
-		System.out.println("");
+		
+		System.out.println("digite o segundo numero:");
 		n2=leia.nextFloat();
-		System.out.println("");
+		
+		System.out.println("digite o terceiro numero:");
 		n3=leia.nextFloat();
-		System.out.println("");
+		
+		System.out.println("digite o quarto numero:");
 		n4=leia.nextFloat();
 		
 		resultado=(n1*n2)-(n3*n4);
